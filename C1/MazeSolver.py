ffd49186fa3c35af5be29c567490c99bd5394f43
Jed Rembold <jjrembold@willamette.edu>
2018-12-24 11:36:25 -0800

    Add much better documentation and commenting

--- C1/MazeSolver.py
+++ C1/MazeSolver.py
@@ -1,39 +1,58 @@
+"""
+Solution for Challenge 1a
+ - Jed Rembold
+
+Algorithm uses the follow the right-hand wall
+method to find its way through the maze. Any
+dead-ends paths are left in the path to be
+plotted.
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 import MazeGen
 
+# Set desired size
+SIZE = 100
 
-SIZE = 50
-
+# Generate the maze
 maze, enter, exit = MazeGen.gen_maze(SIZE)
 
+# Path is my list of the followed points
+# Here I initialize it to the starting point
 path = [np.array(enter)]
 
+# My rotation vector to apply a vector rotation to the right
 rot = np.array([[0,1],[-1,0]])
+# My initial heading (downwards)
 heading = np.array((1,0))
 
-count = 0
+# Keep going as long as our latest point isn't the exit
 while any(path[-1] != exit):
+    # Set our current location to the last point
     curr = path[-1]
-    # print('Currently at:', curr)
+    # Determine what direction is "right" of us
     rhead = rot.dot(heading)
-    # print('Right heading is: ', rhead)
+    # Use that direction to determine the "right block" coordinates
     rcoord = curr + rhead
-    # print('Right coord is: ', rcoord)
+
+    # Making our choices
     if not (maze[tuple(rcoord)]):
-        # print('Turning right and going forward')
+        # There is an opening to the right
+        # Turn right and go forward
         path.append(rcoord)
         heading = rhead
     elif not (maze[tuple(curr+heading)]):
-        # print('Going ahead')
+        # There is an opening ahead of us
+        # Go forward
         path.append(curr+heading)
     else:
-        # print('Rotating left')
+        # Both ahead and to the right are blocked
+        # Turn left
         heading=heading.dot(rot)
 
-    # count += 1
 
-
+# Plot up the resulting path. Using 'gray_r' for black walls
 plt.imshow(maze, cmap='gray_r')
 plt.axis('off')
 plt.plot([x[1] for x in path], [x[0] for x in path], lw=200/SIZE, alpha=0.5)
